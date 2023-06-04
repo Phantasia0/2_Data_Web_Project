66715c6e711fd1d8db48a76bcfb9c2dc6c420688
Phantasia0 <ncprog1@gmail.com>
2023-06-05 01:26:30 +0900

    front-home/about페이지 완료- 미디어쿼리 적용

---
+++ front/src/components/AboutGreenLife.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const AboutGreenLife = () => {
+  return <div></div>;
+};
+
+AboutGreenLife.propTypes = {};
+
+export default AboutGreenLife;
