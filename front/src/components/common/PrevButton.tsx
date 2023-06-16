45722da010a797495e616e8cf0258640573b1a05
[AI7기] 이혜연 <dlgpdusls2@gmail.com>
2023-06-16 17:57:22 +0900

    front - 뒤로가기 버튼 크기, 색상 수정

--- front/src/components/common/PrevButton.tsx
+++ front/src/components/common/PrevButton.tsx
@@ -1,13 +1,29 @@
+import { useTheme } from '@mui/material/styles';
 import { Button } from "@mui/material";
 const PrevButton = () => {
+  const theme = useTheme();
+
   return (
     <Button
       sx={{
-        backgroundColor: "primary.main",
-        color: "white",
-        "&:hover": {
-          backgroundColor: "green",
-          color: "white",
+        backgroundColor: 'primary.main',
+        color: 'white',
+        fontSize: '0.8rem',
+        [theme.breakpoints.only('xs')]: {
+          width: '2rem',
+          height: '1rem',
+        },
+        [theme.breakpoints.only('sm')]: {
+          width: '2rem',
+          height: '1rem',
+        },
+        [theme.breakpoints.only('md')]: {
+          width: '2rem',
+          height: '2rem',
+        },
+        '&:hover': {
+          backgroundColor: '#13ab7e',
+          color: 'white',
         },
       }}
       onClick={() => window.history.back()}
